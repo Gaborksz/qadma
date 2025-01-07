851a2d5c96a633456a62c11627e6bd5e297ad260
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-01-07 17:20:02 +0100

    implement findById method using optionals

--- src/main/java/com/practise/qadma/service/InspectionPlanChangeNoteServiceImpl.java
+++ src/main/java/com/practise/qadma/service/InspectionPlanChangeNoteServiceImpl.java
@@ -2,8 +2,11 @@ package com.practise.qadma.service;
 
 import com.practise.qadma.dao.InspectionPlanChangeNoteRepository;
 import com.practise.qadma.entity.InspectionPlanChangeNote;
+import com.practise.qadma.exception.ItemNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class InspectionPlanChangeNoteServiceImpl implements InspectionPlanChangeNoteService {
@@ -18,7 +21,12 @@ public class InspectionPlanChangeNoteServiceImpl implements InspectionPlanChange
 
     @Override
     public InspectionPlanChangeNote findById(long id) {
-        return inspectionPlanChangeNoteRepository.findById(id);
+
+        Optional<InspectionPlanChangeNote> inspectionPlanChangeNote = inspectionPlanChangeNoteRepository.findById(id);
+
+        if (inspectionPlanChangeNote.isEmpty()) throw new ItemNotFoundException(id, "InspectionPlanChangeNote");
+
+        return inspectionPlanChangeNote.get();
     }
 
     @Override
