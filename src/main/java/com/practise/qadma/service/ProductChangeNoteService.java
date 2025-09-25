54218a82dedf16aa80058e0b499f439b75a5ceb9
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-09-25 15:53:16 +0200

    remove mapping service, implement post ProductChangeNote in all layers

--- src/main/java/com/practise/qadma/service/ProductChangeNoteService.java
+++ src/main/java/com/practise/qadma/service/ProductChangeNoteService.java
@@ -1,12 +1,13 @@
 package com.practise.qadma.service;
 
 import com.practise.qadma.entity.ProductChangeNote;
+import com.practise.qadma.payload.ProductChangeNoteDTO;
 
 import java.util.Set;
 
 public interface ProductChangeNoteService {
 
-    ProductChangeNote save(ProductChangeNote productChangeNote);
+    ProductChangeNoteDTO save(ProductChangeNoteDTO productChangeNote);
 
     Set<ProductChangeNote> getProductChangeNotes(Set<Long> ids);
 }
--- src/main/java/com/practise/qadma/service/ProductChangeNoteServiceImpl.java
+++ src/main/java/com/practise/qadma/service/ProductChangeNoteServiceImpl.java
@@ -1,21 +1,18 @@
 package com.practise.qadma.service;
 
-import com.practise.qadma.auth.dao.QadmaUserRepository;
-import com.practise.qadma.auth.entity.QadmaUser;
 import com.practise.qadma.auth.service.QadmaUserService;
 import com.practise.qadma.dao.ProductChangeNoteRepository;
+import com.practise.qadma.entity.ArchivedProduct;
 import com.practise.qadma.entity.Product;
 import com.practise.qadma.entity.ProductChangeNote;
-import com.practise.qadma.exception.ItemNotFoundException;
+import com.practise.qadma.payload.ProductChangeNoteDTO;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class ProductChangeNoteServiceImpl implements ProductChangeNoteService {
@@ -23,19 +20,34 @@ public class ProductChangeNoteServiceImpl implements ProductChangeNoteService {
     private final ProductChangeNoteRepository productChangeNoteRepository;
     private final QadmaUserService qadmaUserService;
     private final ProductService productService;
+    private final ArchivedProductService archivedProductService;
+    private final ModelMapper modelMapper;
+
 
     @Autowired
-    public ProductChangeNoteServiceImpl(ProductChangeNoteRepository productChangeNoteRepository, QadmaUserService qadmaUserService, ProductService productService) {
+    public ProductChangeNoteServiceImpl(ProductChangeNoteRepository productChangeNoteRepository, QadmaUserService qadmaUserService, ProductService productService, ArchivedProductService archivedProductService, ModelMapper modelMapper) {
         this.productChangeNoteRepository = productChangeNoteRepository;
         this.qadmaUserService = qadmaUserService;
         this.productService = productService;
+        this.archivedProductService = archivedProductService;
+        this.modelMapper = modelMapper;
     }
 
 
+    @Transactional
     @Override
-    public ProductChangeNote save(ProductChangeNote productChangeNote) {
+    public ProductChangeNoteDTO save(ProductChangeNoteDTO dto) {
 
-        return productChangeNoteRepository.save(productChangeNote);
+        ProductChangeNote changeNote = modelMapper.map(dto, ProductChangeNote.class);
+
+        Product managedProduct = productService.save(changeNote.getProduct());
+        ArchivedProduct archivedProduct = archivedProductService.archiveProduct(managedProduct);
+
+        changeNote.setProduct(managedProduct);
+        changeNote.setArchivedProduct(archivedProduct);
+
+        productChangeNoteRepository.save(changeNote);
+        return modelMapper.map(changeNote, ProductChangeNoteDTO.class);
     }
 
     @Override
@@ -56,17 +68,17 @@ public class ProductChangeNoteServiceImpl implements ProductChangeNoteService {
 
     public Set<ProductChangeNote> populateUserFields(Set<ProductChangeNote> productChangeNotes) {
 
-        Set<Long> userIds = productChangeNotes.stream()
-                .flatMap(p -> Stream.of(p.getCreatorId()))
-                .collect(Collectors.toSet());
-
-        Map<Long, QadmaUser> userMap = qadmaUserService.findUsersByIds(userIds).stream()
-                .collect(Collectors.toMap(QadmaUser::getId, user -> user));
-
-        productChangeNotes.forEach(p -> {
-            long creatorId = p.getCreatorId();
-            p.setCreatedBy(userMap.get(creatorId));
-        });
+//        Set<Long> userIds = productChangeNotes.stream()
+//                .flatMap(p -> Stream.of(p.getCreatorId()))
+//                .collect(Collectors.toSet());
+//
+//        Map<Long, QadmaUser> userMap = qadmaUserService.findUsersByIds(userIds).stream()
+//                .collect(Collectors.toMap(QadmaUser::getId, user -> user));
+//
+//        productChangeNotes.forEach(p -> {
+//            long creatorId = p.getCreatorId();
+//            p.setCreatedBy(userMap.get(creatorId));
+//        });
 
         return productChangeNotes;
     }
