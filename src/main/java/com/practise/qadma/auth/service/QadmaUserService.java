42341160f49ecbe0c93ac83f9f9f21cc6a629eb6
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-10-24 22:42:30 +0200

    implement QamaUser findUserById(long id) throws UserNotFoundException;

--- src/main/java/com/practise/qadma/auth/service/QadmaUserService.java
+++ src/main/java/com/practise/qadma/auth/service/QadmaUserService.java
@@ -1,6 +1,7 @@
 package com.practise.qadma.auth.service;
 
 import com.practise.qadma.auth.entity.QadmaUser;
+import com.practise.qadma.exception.UserNotFoundException;
 
 import java.util.Set;
 
@@ -8,7 +9,7 @@ public interface QadmaUserService {
 
     Set<QadmaUser> findUsersByIds(Set<Long> userIdSet);
 
-    QadmaUser findUserById(long id);
+    QadmaUser findUserById(long id) throws UserNotFoundException;
 
     Set<QadmaUser> findUsersBySearchTerm(String searchTerm);
 }
